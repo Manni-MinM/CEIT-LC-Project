defdf18df4f292668776e337d25dc4b4d8d8fce4
manni-minm <mannimoghimi@gmail.com>
2022-01-05 20:57:22 +0330

    bug fixes

--- module_3/exit_parking_lot.v
+++ module_3/exit_parking_lot.v
@@ -23,11 +23,11 @@ module exit_parking_lot(
 	park_location);
 	input [2:0] park_number;
 	output [7:0] park_location;
-	assign park_location = (park_number == 3'b000 ? 8'b00000001 :
-							park_number == 3'b001 ? 8'b00000010 :
-							park_number == 3'b010 ? 8'b00000100 :
-							park_number == 3'b011 ? 8'b00001000 :
-							park_number == 3'b100 ? 8'b00010000 :
-							park_number == 3'b101 ? 8'b00100000 :
-							park_number == 3'b110 ? 8'b01000000 : 8'b10000000) ;
+	assign park_location = (park_number == 3'b000 ? 8'b11111110 :
+							park_number == 3'b001 ? 8'b11111101 :
+							park_number == 3'b010 ? 8'b11111011 :
+							park_number == 3'b011 ? 8'b11110111 :
+							park_number == 3'b100 ? 8'b11101111 :
+							park_number == 3'b101 ? 8'b11011111 :
+							park_number == 3'b110 ? 8'b10111111 : 8'b01111111) ;
 endmodule
