a456f927123521daf78ff04ae2ce13909d02c7c6
manni-minm <mannimoghimi@gmail.com>
2022-01-05 19:56:31 +0330

    initial commit

---
+++ module_1/entry_checker.v
@@ -0,0 +1,29 @@
+/*--  *******************************************************
+  --  Computer Architecture Course, Laboratory Sources 
+  --  Amirkabir University of Technology (Tehran Polytechnic)
+  --  Department of Computer Engineering (CE-AUT)
+  --  https://ce[dot]aut[dot]ac[dot]ir
+  --  *******************************************************
+  --  All Rights reserved (C) 2021-2022
+  --  *******************************************************
+  --  Student ID  : 9931054 & 9931066
+  --  Student Name: Manni Moghimi & Amirhossein Iravanimanesh
+  --  Student Mail: 
+  --  *******************************************************
+  --  Additional Comments:
+  --
+  --*/
+
+/*-----------------------------------------------------------
+  ---  Module Name: entry_checker 
+  -----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module entry_checker(
+	entry,
+	parking_capacity,
+	enable);
+	input entry;
+	input [7:0] parking_capacity;
+	output enable;
+	assign enable = (entry ? parking_capacity[0] | parking_capacity[1] | parking_capacity[2] | parking_capacity[3] | parking_capacity[4] | parking_capacity[5] | parking_capacity[6] | parking_capacity[7] : 7'bzzzzzzz) ;
+endmodule
---
+++ module_1/park_space_number.v
@@ -0,0 +1,37 @@
+/*--  *******************************************************
+  --  Computer Architecture Course, Laboratory Sources 
+  --  Amirkabir University of Technology (Tehran Polytechnic)
+  --  Department of Computer Engineering (CE-AUT)
+  --  https://ce[dot]aut[dot]ac[dot]ir
+  --  *******************************************************
+  --  All Rights reserved (C) 2021-2022
+  --  *******************************************************
+  --  Student ID  : 9931054 & 9931066
+  --  Student Name: Manni Moghimi & Amirhossein Iravanimanesh
+  --  Student Mail: 
+  --  *******************************************************
+  --  Additional Comments:
+  --
+  --*/
+
+/*-----------------------------------------------------------
+  ---  Module Name: park_space_number 
+  -----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module park_space_number(
+	enable,
+	parking_capacity,
+	park_number);
+	input enable;
+	input [7:0] parking_capacity;
+	output [2:0] park_number;
+	assign park_number = (parking_capacity[0] == 1'b1 ? 3'b000 :
+							parking_capacity[1] == 1'b1 ? 3'b001 :
+							parking_capacity[2] == 1'b1 ? 3'b010 :
+							parking_capacity[3] == 1'b1 ? 3'b011 :
+							parking_capacity[4] == 1'b1 ? 3'b100 :
+							parking_capacity[5] == 1'b1 ? 3'b101 :
+							parking_capacity[6] == 1'b1 ? 3'b110 : 
+							parking_capacity[7] == 1'b1 ? 3'b111 : 3'bzzz) ;
+	
+endmodule
